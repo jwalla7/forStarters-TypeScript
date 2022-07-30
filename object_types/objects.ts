6acfe979a7670fb190b2d75263dccdac6cbb57b1
jwalla7 <j7wallace7@outlook.com>
2022-07-29 20:10:43 -0500

    Jul 29 0808

--- object_types/objects.ts
+++ object_types/objects.ts
@@ -1,3 +1,19 @@
+// Generic, not best practice
+const basketball_team: {
+    name: string;
+    roster_cap: number;
+} = {
+    name: "Lakers",
+    roster_cap: 166126381.00
+}
+
+// Generic, not best practice
+const animal: object = {
+    name: "lion",
+    habitat: "jungle"
+};
+
+// Better Practice
 const person = {
     name: "Jeremiah",
     age: 31
