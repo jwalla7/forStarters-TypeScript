81abccd3e3a75da9f6fa9b07ffc712aad1e323cd
jwalla7 <j7wallace7@outlook.com>
2022-08-02 16:01:33 -0500

    Aug 2 0359

--- aliases_types/aliases.ts
+++ aliases_types/aliases.ts
@@ -6,23 +6,3 @@
  * https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#type-aliases
  * 
  */
-
-type BasketballShotPoints = {
-    jump_shot: '2' | 2;
-    free_throw: '1' | 1;
-    layup: '2' | 2;
-    reverse_layup: '2' | 2;
-    hook_shot: '2' | 2;
-    fade_away: '2' | 2;
-    dunk: '2' | 2;
-    reverse_dunk: '2' | 2;
-    three_point_shot: '3' | 3;
-};
-
-type recordShot = 'as-number' | 'as-text';
-
-function playerPointsPerGame(shot_type: BasketballShotPoints, recordShot) {
-
-}
-
-
